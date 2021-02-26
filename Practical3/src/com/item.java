d37d7487abf2f99422ab1df51197cef4f4de1468
Pasan's Pc <pasancbandara@gmail,.com>
2021-02-26 18:08:49 +0530

    first commit

---
+++ Practical3/src/com/item.java
@@ -0,0 +1,22 @@
+package com;
+import java.sql.*;
+public class item {
+
+	public Connection connect(){
+		 Connection con = null;
+	
+		 try{
+			 Class.forName("com.mysql.jdbc.Driver");
+			 con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root", "root");
+			 //For testing
+			 System.out.print("Successfully connected");
+		 }
+		 catch(Exception e){
+			 e.printStackTrace();
+		 }
+	
+		 return con;
+	}
+
+	
+}
